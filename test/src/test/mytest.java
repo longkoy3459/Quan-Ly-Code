409a781b6484488f8207e23f4a12fa0a373c8186
longkoy <longkoy0409@gmail.com>
2019-04-04 23:41:03 +0700

    sua lan 1

--- test/src/test/mytest.java
+++ test/src/test/mytest.java
@@ -5,6 +5,8 @@ public class mytest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helo ") ;
+		System.out.println("helo long ") ;
+
 
 	}
 
